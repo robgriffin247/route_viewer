56146614c04e701e31ae56a2f8951c8da0be9a37
robgriffin247 <robgriffin247@hotmail.com>
2024-10-04 12:54:26 +0200

    added columns to dropdown and laps

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 import streamlit as st
 import duckdb
-import os
 import plotly.graph_objects as go
-#from dotenv import load_dotenv
 
-#load_dotenv()
-#load_dotenv('variables.env')
+col1, col2, col3 = st.columns(3)
 
-col1, col2 = st.columns(2)
-
-#with duckdb.connect(os.getenv("DB")) as con:
 with duckdb.connect("data/data.duckdb") as con:
+    # User input to select world
     worlds = con.sql(f"""SELECT WORLD FROM INTERMEDIATE.OBT_FIT""").to_df()
     world = col1.selectbox("World", worlds.world.unique(), index=len(list(worlds.world.unique()))-1)
 
+    # User input to select route (as a child of world)
     routes = con.sql(f"""SELECT ROUTE FROM INTERMEDIATE.OBT_FIT WHERE WORLD='{world}'""").to_df()
     route = col2.selectbox("Route", routes.route.unique())
 
-#with duckdb.connect(os.getenv("DB")) as con:
-with duckdb.connect("data/data.duckdb") as con:
-    fit_data = con.sql(f"""SELECT * FROM INTERMEDIATE.OBT_FIT WHERE WORLD='{world}' AND ROUTE='{route}'""").to_df()
-            
-    route_profile = go.Figure()
+    # User input to set the number of laps to generate
+    col3.number_input("Laps", min_value=1, max_value=1, value=1, step=1)
 
-    route_profile.add_trace(go.Scatter(x=fit_data["distance"], y=fit_data["altitude"], mode="lines", 
-                                    #customdata=dt[["distance_fmt", "altitude_fmt"]],
-                                    #hovertemplate="<b>Distance: %{customdata[0]}km</b><br>" + 
-                                    #    "<b>Altitude: %{customdata[1]}m</b><br>" + 
-                                    #    "<extra></extra>"
-    ))
+    # Get the route data
+    fit_data = con.sql(f"""SELECT * 
+                       FROM INTERMEDIATE.OBT_FIT 
+                       WHERE WORLD='{world}' AND ROUTE='{route}'""").to_df()
 
-    route_profile.update_layout(xaxis_title="Distance (km)", yaxis_title="Altitude (m)")
+# Frontend:        
+route_profile = go.Figure()
 
-    st.plotly_chart(route_profile)
+route_profile.add_trace(go.Scatter(
+    x=fit_data["distance"], 
+    y=fit_data["altitude"], 
+    mode="lines"))
+
+route_profile.update_layout(
+    xaxis_title="Distance (km)", 
+    yaxis_title="Altitude (m)")
+
+st.plotly_chart(route_profile)
