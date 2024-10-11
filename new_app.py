afaddf05821a23de5919800ba10bad6ab5790be5
robgriffin247 <robgriffin247@hotmail.com>
2024-10-11 08:45:27 +0200

    added highlights to fig

--- new_app.py
+++ new_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px 
 
 from app_files.get_data import get_fit, get_notes
-from app_files.visualise_data import create_profile_plot, create_notes_table
+from app_files.visualise_data import create_output
 
 st.set_page_config(
     page_title="RouteViewer 2.0",
@@ -21,7 +21,13 @@ focal_route_data = get_fit(world=world_value, route=route_value, metric=metric_v
 notes_data = get_notes(world=world_value, route=route_value, metric=metric_value)
 
 # CREATE OUTPUTS
-profile_plot = create_profile_plot(data=focal_route_data, metric=metric_value)
-notes_table = create_notes_table(data=notes_data)
+output = create_output(data=focal_route_data, notes=notes_data, metric=metric_value)
+
 
 # TODO: Work on notes table
+# - empty segment if not changed
+# - add laps feature
+# - allow custom highlighting
+# TODO: Cut route data to finish line
+
+
