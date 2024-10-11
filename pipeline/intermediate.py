e38730aa2c64eba63a47b176bd42f7f9df31ab71
robgriffin247 <robgriffin247@hotmail.com>
2024-10-11 16:25:56 +0200

    complete int_fits to give distance and altitude as columns of imperial and metric, formatted, and gradient

--- pipeline/intermediate.py
+++ pipeline/intermediate.py
@@ -6,33 +6,47 @@ def int_fits():
         con.sql(f"CREATE SCHEMA IF NOT EXISTS {os.getenv('INT_SCHEMA')}")
 
         fit_df = con.sql(f"""
-                WITH SOURCE AS (SELECT * FROM {os.getenv('STG_SCHEMA')}.stg_fits),
-                SELECT_COLS AS (
-                    SELECT world, route, distance, altitude FROM SOURCE                      
-                ),
-                CONVERT AS (
-                    SELECT *,
-                        distance/1000 AS distance_km,
-                        distance/1609.344 AS distance_miles,
-                        altitude/0.3048 AS altitude_ft
-                    FROM SOURCE
-                ),
-                FORMAT AS (
-                    SELECT *
-                    FROM CONVERT
-                ),
-                CHANGE AS (
-                    SELECT * 
-                    FROM FORMAT),
-                GRADIENT AS (
-                    SELECT *
-                    FROM CHANGE)
+                WITH 
+                    SOURCE AS (SELECT * FROM {os.getenv('STG_SCHEMA')}.stg_fits),
+                    
+                    SELECT_COLS AS (
+                        SELECT world, route, distance, altitude FROM SOURCE                      
+                    ),
 
-                SELECT * FROM GRADIENT""").to_df()
+                    CONVERT AS (
+                        SELECT world, route, distance, altitude,
+                            distance/1000 AS distance_met,
+                            distance/1609.344 AS distance_imp,
+                            altitude AS altitude_met,
+                            altitude/0.3048 AS altitude_imp
+                        FROM SOURCE
+                    ),
+
+                    FORMAT AS (
+                        SELECT *,
+                            CONCAT(CAST(ROUND(CAST(distance_met AS DECIMAL(10,2)), 2) as varchar), ' km') AS distance_met_fmt,
+                            CONCAT(CAST(ROUND(CAST(distance_imp AS DECIMAL(10,2)), 2) as varchar), ' miles') AS distance_imp_fmt,
+                            CONCAT(CAST(ROUND(CAST(altitude_met AS DECIMAL(10,2)), 2) as varchar), ' m') AS altitude_met_fmt,
+                            CONCAT(CAST(ROUND(CAST(altitude_imp AS DECIMAL(10,2)), 2) as varchar), ' ft') AS altitude_imp_fmt
+                        FROM CONVERT
+                    ),
+
+                    CHANGE AS (
+                        SELECT *,
+                            CASE WHEN ROW_NUMBER() OVER() = 1 THEN 0 ELSE distance - LAG(distance) OVER() END AS distance_delta,
+                            CASE WHEN ROW_NUMBER() OVER() = 1 THEN 0 ELSE altitude - LAG(altitude) OVER() END AS altitude_delta
+                        FROM FORMAT),
+
+                    GRADIENT AS (
+                        SELECT *,
+                            CASE WHEN ROW_NUMBER() OVER() = 1 THEN 0 ELSE altitude_delta/distance_delta*100 END AS gradient
+                        FROM CHANGE)
+
+                SELECT * EXCLUDE(distance_delta, altitude_delta) FROM GRADIENT""").to_df()
         
         con.sql(f"CREATE OR REPLACE TABLE {os.getenv('INT_SCHEMA')}.int_fits AS SELECT * FROM fit_df")
 
-        print(con.sql(f"SELECT * FROM {os.getenv('INT_SCHEMA')}.int_fits LIMIT 5"))
+        print(con.sql(f"SELECT * FROM {os.getenv('INT_SCHEMA')}.int_fits LIMIT 50"))
 
 def int_notes():
     print("generating int_notes")
