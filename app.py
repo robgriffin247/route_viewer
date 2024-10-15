44d508017b9e028f3e99905e7a2ad69879ef23d5
robgriffin247 <robgriffin247@hotmail.com>
2024-10-15 10:40:48 +0200

    added footer and buycoffee

--- app.py
+++ app.py
@@ -19,3 +19,20 @@ lap_menu()
 get_notes()
 get_plot()
 profile_plot_cont.plotly_chart(st.session_state["profile_plot"])
+
+
+st.html("""
+<style>
+    .subtle { color: #4f6360; 
+        font-weight: 300;}
+    .subtle_link { color: #478f84; }
+</style>
+<br/>
+<br/>
+<hr/>
+<p class="subtle">RouteViewer is developed by Rob Griffin &mdash; data engineer by day and keen zwifter by night! Follow me on instagram <a class="subtle_link" href=="https://www.instagram.com/griffin_cycling/">@griffin_cycling</a> for updates.</p>
+            
+<p class="subtle">It is early days for this project and I am currently working to build the data, but it takes time &mdash; each route requires riding and annotating. This involves riding the route myself or sending a bot around the route to get <em>.fit</em> data, and trawling through race videos on YouTube to develop notes. It takes time and money!</p>
+
+<p class="subtle">To support the project, to help cover costs (like the hardware and Zwift membership needed to run bots) or just to give me a bit of motivation, then head over to <a class="subtle_link" href="https://buymeacoffee.com/griffin_cycling">buymeacoffee</a>!</p>
+""")
