43efa73cf129f16f38d109c954928427e1c9f8c8
robgriffin247 <robgriffin247@hotmail.com>
2024-11-25 20:47:16 +0100

    testing with mm route

--- webapp_files/index.py
+++ webapp_files/index.py
@@ -5,8 +5,8 @@ from webapp_functions.user_input_settings import user_input_settings
 from webapp_functions.notes_table import notes_table
 from webapp_functions.profile_plot import profile_plot
 
-st.session_state["default_world"] = "Watopia"
-st.session_state["default_route"] = "Oh Hill No"
+st.session_state["default_world"] = "New York"
+st.session_state["default_route"] = "Mighty Metropolitan"
 
 load_data()
 user_input_route()
